5696b2f8a885281d31a56ab1606714be067430d3
Aaryan Sinha <71816639+aaryansinha16@users.noreply.github.com>
2022-10-01 01:26:00 +0530

    Update api.js

--- src/API/api.js
+++ src/API/api.js
@@ -1,15 +1,15 @@
 import axios from "axios"
 
 export const getProducts = (page=1, limit=6, filter) => {
-    return axios.get(`https://powerful-savannah-88617.herokuapp.com/products?_page=${page}&_limit=${limit}&_sort=price&_order=${filter}`)
+    return axios.get(`https://shop-api-gqqn.onrender.com/products?_page=${page}&_limit=${limit}&_sort=price&_order=${filter}`)
 }
 
 export const getTotalPages = () => {
-    return axios.get(`https://powerful-savannah-88617.herokuapp.com/products`)
+    return axios.get(`https://shop-api-gqqn.onrender.com/products`)
 }
 
 export const addToCart = (name, brand,price, description ,rating ,number_of_reviews,available, image) => {
-    return axios.post(`https://powerful-savannah-88617.herokuapp.com/cart`, {
+    return axios.post(`https://shop-api-gqqn.onrender.com/cart`, {
         name, 
         brand,
         price,
@@ -22,9 +22,9 @@ export const addToCart = (name, brand,price, description ,rating ,number_of_revi
 }
 
 export const getCart = () => {
-    return axios.get(`https://powerful-savannah-88617.herokuapp.com/cart`)
+    return axios.get(`https://shop-api-gqqn.onrender.com/cart`)
 }
 
 export const deleteCart = (id) => {
-    return axios.delete(`https://powerful-savannah-88617.herokuapp.com/cart/${id}`)
+    return axios.delete(`https://shop-api-gqqn.onrender.com/cart/${id}`)
 }
